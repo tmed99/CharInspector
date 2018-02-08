ec348e26e016b287b810e612c0174c81b7f9750d
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-07 17:52:52 -0800

    Tells about character.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,22 @@ using namespace std;
 
 int main()
 {
-
+  char letter;
   cout<<"What character do you want to know about?\n";
-
-  //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
-
-  //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
-
-  //in all other cases...
+  cin>> letter;
+  if ( letter >= 'A' && letter <= 'Z')
+  {
+  cout<<""<<letter<<" is an upper case letter!\n";
+  }
+  if ( letter >= 'a' && letter <= 'z')
+  {
+  cout<<""<<letter<<" is a lower case letter!\n";
+  }
+  else
+  {
   cout<<"?! Pssh. What are you talking about?\n";
-  
-  //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  }
+  cout<<"The ASCII value is: "<<(int)letter<<"endl";
 
   return 0;
 }

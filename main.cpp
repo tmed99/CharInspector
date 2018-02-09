e1c9e097eb55c0af92068d97a650d7122ed7075e
tmed99 <tmedina-mercado@ecc-jaguar.csuchico.edu>
2018-02-08 20:59:25 -0800

    Describes character

--- main.cpp
+++ main.cpp
@@ -18,9 +18,9 @@ int main()
   }
   else
   {
-  cout<<"?! Pssh. What are you talking about?\n";
+  cout<<"$?! Pssh. What are you talking about?\n";
   }
-  cout<<"The ASCII value is: "<<(int)letter<<".";cout<<endl;
+  cout<<"The ASCII value is: "<<(int)letter<<"";cout<<endl;
 
   return 0;
 }
